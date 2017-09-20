10aba8ee73cb0d34c086818d4837a9cd964d8b54
Craig Willis <cmwillis02@gmail.com>
2017-09-20 13:34:41 -0400

    Modified franchise_list HTML template

--- contracts/views.py
+++ contracts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Franchise, Player, Contract
 
 # Create your views here.
 
 def franchise_list(request):
-    return render(request, 'contracts/franchise_list.html', {})
+	
+	franchises= Franchise.objects.all()
+	return render(request,'contracts/franchise_list.html',{'franchises': franchises} )
