4f14bce710a7a67f3fca4e8ceeb0f18cfb9ee146
Craig Willis <cmwillis02@gmail.com>
2017-09-20 15:13:16 -0400

    added franchise detail page

--- contracts/views.py
+++ contracts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Franchise, Player, Contract
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -7,3 +8,8 @@ def franchise_list(request):
 	
 	franchises= Franchise.objects.all()
 	return render(request,'contracts/franchise_list.html',{'franchises': franchises} )
+
+def franchise_detail(request, pk):
+    franchise = get_object_or_404(Franchise, pk=pk)
+    return render(request, 'contracts/franchise_detail.html', {'franchise': franchise})	
+	
