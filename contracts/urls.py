e60763d9a4360a3393f410a02a58289c5cf1a015
Craig Willis <cmwillis02@gmail.com>
2017-09-20 13:15:16 -0400

    Added HTML templates/contracts/franchise_list.html

--- contracts/urls.py
+++ contracts/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
-
 from . import views
 
-
 urlpatterns = [
-
+	url(r'^$', views.franchise_list, name='franchise_list'),
 ]
--- contracts/views.py
+++ contracts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def franchise_list(request):
+    return render(request, 'contracts/franchise_list.html', {})
