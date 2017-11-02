60a2f2fdf7813355ebb9bec109a7d275fe751be6
Craig Willis <cmwillis02@gmail.com>
2017-11-02 01:49:46 +0000

    <bug fix> removed parameter call from enter_new_contract method

--- Manage_DB/update_roster.py
+++ Manage_DB/update_roster.py
@@ -25,105 +25,105 @@ except:
 	print ('Unable to connect')
 	sys.exit(1)
 
-	
+
 # -- FUNCTION AND CLASS DEFINITIONS -- #
 
 def create_json(server,type, year, league_id):
 
 	url= 'http://www' + str(server) + '.myfantasyleague.com/' + str(year) + '/export?TYPE=' + type + '&L=' + str(league_id) + '&APIKEY=&FRANCHISE=&JSON=1'
-		
+
 	uh= urlopen(url)
 	data= uh.read()
 	js=json.loads(data)
-	
+
 	return js
-	
+
 def get_current_contracts():
 	#Create list of players with a current contract
-	
+
 	cur.execute(
 				'SELECT player_id, franchise_id, id FROM contracts_contract WHERE current_ind = true'
 				)
 	current_contracts = cur.fetchall()
 	return current_contracts
-	
+
 def close_current_contract(contract_id):
 		# Close a contract with current_ind = 'Y'
-		
+
 		cur.execute(
 					"UPDATE contracts_contract SET current_ind = %s, date_terminated = %s WHERE id = %s", ('N', datetime.date.today(), contract_id)
 					)
-		
+
 def close_remaining_contracts(current_contracts, processed_contracts):
 	# Close all contracts that are no longer current
-	
+
 	for player, franchise, id in current_contracts:
-		
+
 		if id not in processed_contracts:
 			close_current_contract(id)
-				
+
 
 
 class contract(object):
 	"""
 	Class for processing incomming contracts method to set the status, enter the information as a new contract, and update the processed contract list.
 	"""
-	
-	
+
+
 	def __init__(self, player_id, franchise_id, years):
-	
+
 		self.player_id= player_id
 		self.franchise_id= franchise_id
 		self.years= years
 		self.status= ''
 		self.current_contract_id= None
-		
+
 	def set_status(self):
-	
+
 		# Set status = 'Current or 'Edit'
 		for player_id, franchise_id, id in current_contracts:
-			
+
 			if player_id == int(self.player_id):
 				if str(franchise_id) == str(self.franchise_id):
 					self.status= 'Current'
-					
+
 					cur.execute(
 								"SELECT id FROM contracts_contract WHERE player_id= %s AND franchise_id= %s AND current_ind = 'Y'", (self.player_id, self.franchise_id)
 								)
 					self.current_contract_id= cur.fetchone()[0]
-					
+
 					break
-				
+
 				else:
 					self.status= 'Edit'
-					
+
 					cur.execute(
 								"SELECT id FROM contracts_contract WHERE player_id= %s AND current_ind = 'Y'", (self.player_id,)
 								)
 					self.current_contract_id= cur.fetchone()[0]
-					
+
 					break
-			
+
 			else:
 				self.status= 'New'
-				
-		
+
+
 	def enter_new_contract(self):
-		
+
 		# Default all new contracts to 0 years.
 		self.years= 0
 		cur.execute(
 					'INSERT INTO contracts_contract (current_ind, date_assigned, franchise_id, player_id, years) VALUES (%s, %s, %s, %s, %s)',('true',datetime.date.today(), self.franchise_id, self.player_id, self.years)
 					)
-		
-		
+
+
 	def update_processed_contract_list(self):
 		# Create a list of contract IDs that are still current, this will be used to close conctract IDs that are no longer valid.
-		
+
 		if self.current_contract_id != None:
 			processed_contracts.append(self.current_contract_id)
-			
-			
+
+
 
 
 
@@ -136,33 +136,33 @@ processed_contracts= []
 # -- Capture JSON of current MFL Roster
 rosters_js= create_json(61,'rosters', 2017, 21676)
 for franchise in range(0,10):
-		
+
 	franchise_id= franchise + 1
 	for item in rosters_js["rosters"]["franchise"][franchise]["player"]:
-		
+
 		player_id= item['id']
 		years= item['contractYear']
-		
+
 		# -- Create contract object for each contract entry and determine the status (Current, Edit, New)
 		contract_entry= contract(player_id, franchise_id, years)
 		contract_entry.set_status()
-		
+
 		# -- Process each contract entry according to status
-		if contract_entry.status == 'Current': 
-			
+		if contract_entry.status == 'Current':
+
 			contract_entry.update_processed_contract_list()
-		
+
 		elif contract_entry.status == 'New':
-			contract_entry.enter_new_contract(0)
-			
+			contract_entry.enter_new_contract()
+
 			contract_entry.update_processed_contract_list()
-		
+
 		elif contract_entry.status == 'Edit':
 			close_current_contract(contract_entry.current_contract_id)
 			contract_entry.enter_new_contract(contract_entry.years)
-			
+
 			contract_entry.update_processed_contract_list()
-		
+
 close_remaining_contracts(current_contracts, processed_contracts)
 conn.commit()
 
