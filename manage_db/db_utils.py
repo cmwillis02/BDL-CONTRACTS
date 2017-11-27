6537b01e95692252b08744324af273e74f94c885
Craig Willis <cmwillis02@gmail.com>
2017-11-27 14:24:16 -0500

    Added db_utils program

---
+++ manage_db/db_utils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3.6
+
+import psycopg2
+
+class db_util():
+
+	def __init__(self):
+		
+		self.dsn_database= 'CORE'
+		self.dsn_hostname= 'bdlcompanion.cquxuyvkuxqs.us-east-1.rds.amazonaws.com'
+		self.dsn_port = '5432'
+		self.dsn_uid= 'bdladmin'
+		self.dsn_pwd= 'bdladmin!23'
+
+	def connect(self):
+		
+		try:
+			conn_string= "host={} port={} dbname={} user={} password={}".format(self.dsn_hostname, self.dsn_port, self.dsn_database, self.dsn_uid, self.dsn_pwd)
+			conn= psycopg2.connect(conn_string)
+			cur= conn.cursor()
+			print ('Connected')
+			
+			return cur
+		
+		except:
+			print ('Unable to connect')
+			sys.exit(1)
+			
+	def get_current_week(self):
+	
+		cur= self.connect()
+		
+		cur.execute("SELECT min(week_id) FROM contracts_week WHERE run_status= 0")
+		
+		return cur.fetchall()[0][0]
+		
+test= db_util()
+
+print (test.get_current_week())
