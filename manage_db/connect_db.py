29670be20f9005de7a52e8f0ab9f067326ff387d
Craig Willis <cmwillis02@gmail.com>
2018-02-22 10:26:28 -0500

    Initial Deployment data steps

---
+++ manage_db/connect_db.py
@@ -0,0 +1,30 @@
+import MySQLdb as sqldb
+
+
+class Connect(object):
+	
+	def connect(self):
+		
+		self.db=sqldb.connect(
+						host="localhost",
+						user="root",
+						passwd="Bdladmin!23",
+						db="BDLCORE")
+		self.cur= self.db.cursor()
+		print ("Connected to Dev")
+		
+	def commit(self, message=None):
+		
+		self.db.commit()
+		if message:
+			print ("Commit - {}".format(message))
+		else:
+			print ("Commit to Dev")
+		
+	def view_tables(self):
+		"View all tables in DB connection"
+		
+		self.cur.execute("SHOW TABLES")
+		
+		print (self.cur.fetchall())
+		
